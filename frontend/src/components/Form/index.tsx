8f5d59f5c3f1a119eba3aab806292d73489cd1ac
Rafael Campos <rafaelccamposdev@gmail.com>
2021-05-17 16:30:29 -0300

    ajustando inputs form

--- frontend/src/components/Form/index.tsx
+++ frontend/src/components/Form/index.tsx
@@ -3,11 +3,11 @@ import { TextField, Box, Button, Container } from "@material-ui/core";
 const Form = () => {
     return (
         <Container className="formContainer">
-            <TextField margin="normal" fullWidth label="Nome"/>
-            <TextField margin="normal" fullWidth label="Endereço"/>
-            <TextField margin="normal" fullWidth label="Telefone"/>
-            <TextField margin="normal" fullWidth label="E-mail"/>
-            <TextField margin="normal" fullWidth label="Senha"/>
+            <TextField variant="filled" margin="normal" fullWidth label="Nome"/>
+            <TextField variant="filled" margin="normal" fullWidth label="Endereço"/>
+            <TextField variant="filled" margin="normal" fullWidth label="Telefone"/>
+            <TextField variant="filled" margin="normal" fullWidth label="E-mail"/>
+            <TextField variant="filled" margin="normal" fullWidth label="Senha"/>
             <Box marginTop={2}>
                 <Button variant="contained" type="submit">Cadastrar</Button>
             </Box>
