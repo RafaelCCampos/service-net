132a16dc1ad1b4ff559fbfaa261fec5cf22797cc
Rafael Campos <rafaelccamposdev@gmail.com>
2021-05-24 19:29:56 -0300

    ajustes para deploy backend

---
+++ create.sql
@@ -0,0 +1,2 @@
+create table tb_persons (id bigint not null auto_increment, email varchar(255), endereco varchar(255), nome varchar(255), senha varchar(255), telefone varchar(255), primary key (id)) engine=InnoDB;
+create table tb_persons (id bigint not null auto_increment, email varchar(255), endereco varchar(255), nome varchar(255), senha varchar(255), telefone varchar(255), primary key (id)) engine=InnoDB;
