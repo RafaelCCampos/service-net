aa3885306f59fce350aa29f6b059b76f134c2866
Rafael Campos <rafaelccamposdev@gmail.com>
2021-06-13 19:56:33 -0300

    Ajustando estrutura das pastas

--- src/main/java/com/servicenet/snapi/repository/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.servicenet.snapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.servicenet.snapi.entities.Person;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-
-}
