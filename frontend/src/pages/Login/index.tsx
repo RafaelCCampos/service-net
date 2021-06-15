3fb39754ef69fc46f3fdff63ffaf5d11a8afbdaf
Rafael Campos <rafaelccamposdev@gmail.com>
2021-06-15 00:15:48 -0300

    add funcionalidade de login do user

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -5,20 +5,51 @@ import imgOperarios from '../../assets/images/landing-top.svg';
 
 import './styles.scss';
 import { Link, useHistory } from "react-router-dom";
-import { useRef } from "react";
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+import { Person } from "../../types/cadastro";
 
 const Login = () => {
 
+    useEffect(() => {
+        api.get('/persons')
+        .then(response => {
+            setUsers(response.data)
+        })
+    },[])
+
+    const blankUser: Person = {
+        id: 0,
+        nome: '',
+        endereco: '',
+        telefone: '',
+        email: '',
+        senha: '',
+    }
+
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const [user, setUser] = useState<Person>(blankUser);
+    const [users, setUsers] =  useState<Person[]>([]);
+
     const history = useHistory()
 
-    const refLogin = useRef<HTMLInputElement>(null)
-    const refPassword = useRef<HTMLInputElement>(null)
+    const userLoginChange = (userLogin: string) => {
+        setLogin(userLogin)
+        setUser(users.filter(userFilter => {return userFilter.email === userLogin})[0])
+    }
+    
 
     const loginClick = () => {
-        if (refLogin.current?.value === "admin@servicenet.com" && refPassword.current?.value === "servicenet") {
+        if (login === "admin@servicenet.com" && password === "servicenet") {
             history.push('/usuarios')
         } else {
-            alert("O Login e o password não conferem, verifique os dados inseridos e tente novamente.")
+            if (user?.id > 0 && user.email === login && user.senha === password) {
+                history.push(`/usuarios/${user.id}/servicos`)
+            }
+            else {
+                alert("O Login e o password não conferem, verifique os dados inseridos e tente novamente.")
+            }
         }
     }
 
@@ -29,8 +60,8 @@ const Login = () => {
                 <Logo/>
                 <img src={imgOperarios} width="350px" alt="Trabalhadores"/>
                 <div className="fieldContainer">
-                    <TextField variant="filled" value={refLogin.current?.value} margin="normal" fullWidth label="E-mail" inputRef={refLogin}/>
-                    <TextField type="password" value={refPassword.current?.value} variant="filled" margin="normal" fullWidth label="Senha" inputRef={refPassword}/>
+                    <TextField variant="filled" value={login} onChange={e => userLoginChange(e.target.value)} margin="normal" fullWidth label="E-mail"/>
+                    <TextField type="password" value={password} onChange={e => setPassword(e.target.value)} variant="filled" margin="normal" fullWidth label="Senha"/>
                 </div>
                 <div className="buttonContainer">
                     <Button size="large" variant="contained" onClick={loginClick}>Login</Button>
